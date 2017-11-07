6384b2d989b5864d310d26d06d012fdb69c6d09c
melodyfs <melodyfs@users.noreply.github.com>
2017-11-06 20:44:04 -0800

    Implement user sign up/in in database

---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, request, make_response
+from flask_restful import Resource, Api
+from pymongo import MongoClient
+from utils.mongo_json_encoder import JSONEncoder
+from bson.objectid import ObjectId
+import bcrypt
+import pdb
+
+app = Flask(__name__)
+mongo = MongoClient('localhost', 27017)
+# mongo = MongoClient('mongodb://melodyfs:Melody0116@ds015335.mlab.com:15335/trip_planner_development')
+app.db = mongo.grocery_development
+api = Api(app)
+
+def auth_validation(email, password):
+    user_collection = app.db.users
+    myUser = user_collection.find_one({'email': email})
+    user_password = myUser['password']
+
+    if myUser is None:
+        return ({"error": "Email not found"}, 404, None)
+    else:
+        encodedPassword = password.encode('utf-8')
+        if bcrypt.hashpw(encodedPassword, user_password) == user_password:
+            return True
+        else:
+            return False
+
+def auth_function(func):
+    def wrapper(*args, **kwargs):
+        auth = request.authorization
+        if not auth_validation(auth.username, auth.password):
+            return ({'error': 'Could not verify your credentials'}, 401,
+                    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        return func(*args, **kwargs)
+    return wrapper
+
+
+
+class User(Resource):
+
+    def post(self):
+        new_user = request.json
+        user_collection = app.db.users
+
+        if 'email' in new_user and 'password' in new_user:
+            user = user_collection.find_one({'email': new_user.get('email')})
+            if user:
+                 return ({'error': 'email already exists'}, 409, None)
+            else:
+
+                password = new_user.get('password')
+                app.bcrypt_rounds = 12
+                # Convert password to utf-8 string
+                encodedPassword = password.encode('utf-8')
+                hashed = bcrypt.hashpw(encodedPassword, bcrypt.gensalt(app.bcrypt_rounds))
+                result = user_collection.insert({'email': new_user.get('email'),
+                                                 'password': hashed})
+
+                user.pop(password)
+
+                return (user, 201, None)
+        else:
+            return ({"error": "Can't create user"}, 400, None)
+
+    @auth_function
+    def get(self):
+        user_collection = app.db.users
+        email = request.args.get("email")
+        myUser = user_collection.find_one({'email': email})
+
+        if myUser is None:
+            return ({"error": "Email not found"}, 404, None)
+        else:
+            myUser.pop('password')
+            return (myUser, 200, None)
+
+
+    @auth_function
+    def patch(self):
+        email = request.args.get('email')
+
+        update_ = request.json
+        # new_name = update_.get('name')
+        new_password = update_.get('password')
+        new_email = update_.get('email')
+        user_collection = app.db.users
+
+        if email is not None:
+            user = user_collection.find_one({'email': email})
+            if new_email:
+                 user["email"] = new_email
+            if new_password:
+                user['password'] = new_password
+            user_collection.save(user)
+
+            return (user, 200, None)
+        else:
+            return ({"error": "Can't modify the user"}, 404, None)
+
+    @auth_function
+    def delete(self):
+        user_collection = app.db.users
+        email = request.args.get('email')
+        myUser = user_collection.delete_one({'email': email})
+
+        if myUser is None:
+            return ({"error": "Can't modify the user"}, 404, None)
+        else:
+            return myUser
+
+
+##api routes
+api.add_resource(User,'/users')
+# api.add_resource(Trip,'/user/trips/')
+
+#  Custom JSON serializer for flask_restful
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    resp = make_response(JSONEncoder().encode(data), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+if __name__ == '__main__':
+    app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+    app.run(debug=True)
---
+++ development/lib/python3.6/site-packages/flake8_docstrings.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""Implementation of pydocstyle integration with Flake8.
+
+pydocstyle docstrings convention needs error code and class parser for be
+included as module into flake8
+"""
+import sys
+
+from flake8_polyfill import stdin
+import pycodestyle
+try:
+    import pydocstyle as pep257
+    module_name = 'pydocstyle'
+except ImportError:
+    import pep257
+    module_name = 'pep257'
+
+__version__ = '1.1.0'
+__all__ = ('pep257Checker',)
+
+stdin.monkey_patch('pycodestyle')
+
+
+class EnvironError(pep257.Error):
+    code = 'D998'
+    context = None
+
+    @property
+    def short_desc(self):
+        return sys.exc_info()[1]
+
+
+class AllError(pep257.Error):
+    code = 'D999'
+    short_desc = '__all__ was found to be a list or other mutable collection'
+    context = None
+
+
+class pep257Checker(object):
+    """Flake8 needs a class to check python file."""
+
+    name = 'flake8-docstrings'
+    version = __version__ + ', {0}: {1}'.format(
+        module_name, pep257.__version__
+    )
+
+    STDIN_NAMES = set(['stdin', '-', '(none)', None])
+
+    def __init__(self, tree, filename='(none)', builtins=None):
+        """Placeholder."""
+        self.tree = tree
+        self.filename = filename
+        self.checker = pep257.ConventionChecker()
+        self.load_source()
+
+    def _check_source(self):
+        try:
+            # TODO: Naive fix for `pydocstyle 2.0.0` with default settings.
+            # Should probably add a proper setting so `ignore_decorators` can
+            # be set when caling through the CLI
+            return list(self.checker.check_source(
+                self.source,
+                self.filename,
+                ignore_decorators=None,
+            ))
+        except pep257.AllError as err:
+            return [AllError(err)]
+        except EnvironmentError as err:
+            return [EnvironError(err)]
+
+    def run(self):
+        """Use directly check() api from pydocstyle."""
+        checked_codes = pep257.conventions.pep257
+        for error in self._check_source():
+            if isinstance(error, pep257.Error) and error.code in checked_codes:
+                # NOTE(sigmavirus24): Fixes GitLab#3
+                message = '%s %s' % (error.code, error.short_desc)
+                yield (error.line, 0, message, type(self))
+
+    def load_source(self):
+        """Load the source for the specified file."""
+        if self.filename in self.STDIN_NAMES:
+            self.filename = 'stdin'
+            self.source = pycodestyle.stdin_get_value()
+        else:
+            with pep257.tokenize_open(self.filename) as fd:
+                self.source = fd.read()
