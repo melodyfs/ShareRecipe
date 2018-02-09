a07964c3f476098f77651e3044ea5941eb58ce76
Melody Yang <melody870116@hotmail.com>
2018-02-09 15:09:48 -0800

    change db user

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pdb
 
 app = Flask(__name__)
 # mongo = MongoClient('localhost', 27017)
-mongo = MongoClient('mongodb://test:test@ds261247.mlab.com:61247/heroku_jnnmnxkw')
+mongo = MongoClient('mongodb://test2:test2@ds261247.mlab.com:61247/heroku_jnnmnxkw')
 app.db = mongo.grocery_development
 api = Api(app)
 
